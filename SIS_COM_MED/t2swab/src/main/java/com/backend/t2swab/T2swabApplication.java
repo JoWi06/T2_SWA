0e6d8c691754df7d88078038c4222b27dc5c15aa
WilfredoCenturion <165825241+WilfredoCenturion@users.noreply.github.com>
2026-07-02 11:08:51 -0500

    Carga del backend

---
+++ SIS_COM_MED/t2swab/src/main/java/com/backend/t2swab/T2swabApplication.java
@@ -0,0 +1,13 @@
+package com.backend.t2swab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2swabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2swabApplication.class, args);
+	}
+
+}
---
+++ SIS_COM_MED/t2swab/src/test/java/com/backend/t2swab/T2swabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.t2swab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2swabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
