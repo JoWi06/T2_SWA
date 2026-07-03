e50320d80fc8b8f62851d017ee9f1b96a58d6a65
WilfredoCenturion <165825241+WilfredoCenturion@users.noreply.github.com>
2026-07-03 10:47:03 -0500

    implementar formulario standalone para creacion y edicion de productos

--- SIS_COM_MED/t2swaf/src/app/app.routes.ts
+++ SIS_COM_MED/t2swaf/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './component/product-list/product-list';
+import { ProductFormComponent } from './component/product-form/product-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'product', component: ProductListComponent },
+  { path: 'product/new', component: ProductFormComponent },
+  { path: 'product/edit/:id', component: ProductFormComponent },
+  { path: '', redirectTo: 'product', pathMatch: 'full' },
+  { path: '**', redirectTo: 'product' }
+];
